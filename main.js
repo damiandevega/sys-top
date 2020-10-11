44b01eda1fd12cd045a2196f0f47437f2bf91f62
Damian de Vega <damiandevega@gmail.com>
2020-10-11 11:39:51 -0400

    creating the template and interface styles

--- main.js
+++ main.js
@@ -11,12 +11,11 @@ let mainWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
-    title: 'APP NAME',
-    width: isDev ? 800 : 500,
-    height: 600,
+    title: 'SysTop',
+    width: isDev ? 900 : 355,
+    height: 500,
     icon: `${__dirname}/assets/icons/icon.png`,
     resizable: isDev ? true : false,
-    backgroundColor: 'white',
     webPreferences: {
       nodeIntegration: true,
       enableRemoteModule: true,
