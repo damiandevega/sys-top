6b4bd9a3086170c772113a8dd1ffb25795678115
Damian de Vega <damiandevega@gmail.com>
2020-10-11 15:24:03 -0400

    calculating cpu usage, free space and uptime and displaying to front end

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,22 @@ const { mem } = require('node-os-utils');
 const cpu = osu.cpu;
 const os = osu.os;
 
+// Run every 2 seconds
+setInterval(() => {
+  // CPU Usage
+  cpu.usage().then((info) => {
+    document.getElementById('cpu-usage').innerText = `${info.toFixed(2)}%`;
+  });
+
+  // CPU Free
+  cpu.free().then((info) => {
+    document.getElementById('cpu-free').innerText = `${info.toFixed(2)}%`;
+  });
+
+  // Uptime
+  document.getElementById('sys-uptime').innerText = secondsToDhms(os.uptime());
+}, 2000);
+
 // Set Model
 document.getElementById('cpu-model').innerText = cpu.model();
 
@@ -17,3 +33,14 @@ document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
 mem.info().then((info) => {
   document.getElementById('mem-total').innerText = `${info.totalMemMb} MB`;
 });
+
+// Show days, hours, minutes and seconds
+const secondsToDhms = (seconds) => {
+  seconds = +seconds;
+  const day = Math.floor(seconds / (3600 * 24));
+  const hour = Math.floor((seconds % (3600 * 24)) / 3600);
+  const minute = Math.floor((seconds % 3600) / 60);
+  const second = Math.floor(seconds % 60);
+
+  return `${day}d, ${hour}h, ${minute}m, ${second}s`;
+};
